284c8d7987fab0a8ef23e4e9c6cf8f4871426222
Jesus <csc.jesus@gmail.com>
2025-06-20 10:40:26 -0600

    grupo de rutas

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,26 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::get('/', HomeController::class)->name("home.index");
-// //ruta para listar registros
-// Route::get("/posts",[PostController::class,"index"])->name("posts.index");
-// //ruta mostrar formulario crear registro
-// Route::get("/posts/create",[PostController::class,"create"])->name("posts.create");
-// //ruta guardar registro
-// Route::post("/posts",[PostController::class,"store"])->name("posts.create");
-// // ruta mostrar registro
-// Route::get("/posts/{post}",[PostController::class,"show"])->name("posts.show");
-// //ruta mostrar formulario editar registro
-// Route::get("/posts/{post}/edit",[PostController::class,"edit"])->name("posts.edit");
-// //ruta actualziar registro
-// Route::put("/posts/{post}",[PostController::class,"update"])->name("posts.update");
-// //ruta eliminar registro
-// Route::delete("/posts/{post}",[PostController::class,"destroy"])->name("posts.destroy");
+
+Route::prefix("/posts")->name("posts.")->controller(PostController::class)->group(function () {
+    //ruta para listar registros
+    Route::get("/", "index")->name("index");
+    //ruta mostrar formulario crear registro
+    Route::get("/create", "create")->name("create");
+    //ruta guardar registro
+    Route::post("/","store")->name("create");
+    // ruta mostrar registro
+    Route::get("/{post}", "show")->name("show");
+    //ruta mostrar formulario editar registro
+    Route::get("/{post}/edit", "edit")->name("edit");
+    //ruta actualziar registro
+    Route::put("/{post}", "update")->name("update");
+    //ruta eliminar registro
+    Route::delete("/{post}", "destroy")->name("destroy");
+});
+
+
+
 
 //Route::apiResource('home', HomeController::class);
 
@@ -29,4 +35,4 @@ Route::get('/', HomeController::class)->name("home.index");
 //     ->only(["index", "show"]);
 
 Route::resource('articles', PostController::class)->parameters(['articles' => 'post'])
-->names('posts');
+    ->names('posts');

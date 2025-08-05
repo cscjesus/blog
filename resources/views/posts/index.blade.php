ab84655fe5ca114216a51c53b7be3a7219d9e2a3
Jesus hdez <csc.jesus@gmail.com>
2025-08-05 13:38:28 -0600

    varias directivas condicionales

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,11 +10,19 @@
 
 <body>
     <h1>Aqui se mostrara el listado de posts</h1>
-   <script>
-        const posts = @json($posts);
-        console.log(posts);
-    </script>
-    
+    @if (true)
+        <p>Condicion true</p>
+    @else
+        <p>Condicion false</p>
+    @endif
+
+    @isset($prueba)
+        <p>Existe la variable {{$prueba}}</p>
+    @endisset
+
+    @empty($record)
+        <p>La variable no existe o no tiene un valor asignado</p>
+    @endempty
 </body>
 
 </html>
